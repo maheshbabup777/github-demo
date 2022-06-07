e04f9dd80eaf5be1783bb23b7d8987fa9a4f21d6
maheshbabup777 <93246529+maheshbabup777@users.noreply.github.com>
2022-06-07 12:57:03 +0530

    Update secondfile.py

--- secondfile.py
+++ secondfile.py
@@ -1,2 +1,6 @@
-"hello world"
-this is my first commit
+#!/bin/bash
+
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#"
